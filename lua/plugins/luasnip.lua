0e1efd45e5234dcea58a25437e97f7ad5797549e
lotus-x <isuruimesh@gmail.com>
2023-08-19 00:19:38 +0530

    Added language extends for luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -13,6 +13,9 @@ return {
       luasnip.setup(opts)
 
       require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/vscode" } })
+
+      luasnip.filetype_extend("typescript", { "javascript" })
+      luasnip.filetype_extend("typescriptreact", { "javascriptreact" })
     end,
   },
 }
