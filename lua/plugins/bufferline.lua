c31d770d2e721a3470d460c0e93aa2098fdca0e7
Isuru Herath <isuruimesh@gmail.com>
2023-10-04 09:42:18 +0530

    Added bufferline pick keymap

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,9 @@
 return {
   {
     "akinsho/bufferline.nvim",
+    keys = {
+      { "<leader>bg", "<Cmd>BufferLinePick<CR>", desc = "Pick Bufferline Buffer" },
+    },
     opts = {
       highlights = require("catppuccin.groups.integrations.bufferline").get(),
     },
