b1403c45eb52585cedbd22440deb450b9d9c26a1
lotus-x <isuruimesh@gmail.com>
2025-05-08 15:00:38 +0530

    Added mason workaround

--- lua/plugins/grug.lua
+++ lua/plugins/grug.lua
@@ -1,17 +1,19 @@
 return {
-  "MagicDuck/grug-far.nvim",
-  keys = {
-    {
-      "<leader>sf",
-      function()
-        require("grug-far").grug_far({ prefills = { paths = vim.fn.expand("%") } })
-      end,
-      desc = "Search and replace current word",
-      remap = true,
-      mode = { "v", "n" },
+  {
+    "MagicDuck/grug-far.nvim",
+    keys = {
+      {
+        "<leader>sf",
+        function()
+          require("grug-far").grug_far({ prefills = { paths = vim.fn.expand("%") } })
+        end,
+        desc = "Search and replace current word",
+        remap = true,
+        mode = { "v", "n" },
+      },
     },
-  },
-  opts = {
-    startInInsertMode = false,
+    opts = {
+      startInInsertMode = false,
+    },
   },
 }
