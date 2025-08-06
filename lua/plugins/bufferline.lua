6ac88f6eb4104c9cfcdc5d9e6a5fbbec23e61fd7
lotus-x <isuruimesh@gmail.com>
2025-08-06 09:08:28 +0530

    Enabled bufferline with native tab behavior

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,12 +1,8 @@
 return {
   {
     "akinsho/bufferline.nvim",
-    enabled = false,
-    keys = {
-      { "<leader>bg", "<Cmd>BufferLinePick<CR>", desc = "Pick Bufferline Buffer" },
-    },
     opts = {
-      highlights = require("catppuccin.groups.integrations.bufferline").get(),
+      options = { mode = "tabs" },
     },
   },
 }
