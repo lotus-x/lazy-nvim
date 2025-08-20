b6ccc7a3f5bc1014b7bae9b69bc68ce467f1bb0d
lotus-x <isuruimesh@gmail.com>
2025-08-20 13:09:02 +0530

    Added bufferline fix for catpuccin

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,5 +4,11 @@ return {
     opts = {
       options = { mode = "tabs" },
     },
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
   },
 }
