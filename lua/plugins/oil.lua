89780233d93a680feca821942845aedc2dd3f6b9
lotus-x <isuruimesh@gmail.com>
2025-10-04 16:50:13 +0530

    Updated oil keymaps to support grug

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -18,10 +18,32 @@ return {
         ["_"] = { "actions.open_cwd", mode = "n" },
         ["`"] = { "actions.cd", mode = "n" },
         ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-        ["gs"] = { "actions.change_sort", mode = "n" },
+        -- ["gs"] = { "actions.change_sort", mode = "n" },
         ["gx"] = "actions.open_external",
         ["g."] = { "actions.toggle_hidden", mode = "n" },
         ["g\\"] = { "actions.toggle_trash", mode = "n" },
+        -- create a new mapping, gs, to search and replace in the current directory
+        ["gs"] = {
+          callback = function()
+            -- get the current directory
+            local prefills = { paths = require("oil").get_current_dir() }
+
+            local grug_far = require("grug-far")
+            -- instance check
+            if not grug_far.has_instance("explorer") then
+              grug_far.open({
+                instanceName = "explorer",
+                prefills = prefills,
+                staticTitle = "Find and Replace from Explorer",
+              })
+            else
+              grug_far.get_instance("explorer"):open()
+              -- updating the prefills without clearing the search and other fields
+              grug_far.get_instance("explorer"):update_input_values(prefills, false)
+            end
+          end,
+          desc = "oil: Search in directory",
+        },
       },
       view_options = {
         show_hidden = true,
