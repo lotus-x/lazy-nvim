230a1e73ce5bb9c195838b1990a1377b08530a58
lotus-x <isuruimesh@gmail.com>
2025-10-15 00:32:33 +0530

    Updated lsp config

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -6,21 +6,6 @@ return {
         exclude = { "typescript", "typescriptreact", "vue" },
       },
       servers = {
-        -- lua_ls = {
-        --   settings = {
-        --     Lua = {
-        --       workspace = {
-        --         checkThirdParty = false,
-        --       },
-        --       completion = {
-        --         callSnippet = "Replace",
-        --       },
-        --     },
-        --   },
-        -- },
-        -- vimls = {},
-        cssls = {},
-        cssmodules_ls = {},
         yamlls = {
           settings = {
             yaml = {
@@ -59,54 +44,6 @@ return {
             },
           },
         },
-        gopls = {
-          settings = {
-            gopls = {
-              analyses = {
-                -- unreachable = true,
-                -- nilness = true,
-                -- unusedparams = true,
-                -- useany = true,
-                -- unusedwrite = true,
-                -- ST1003 = true,
-                -- undeclaredname = true,
-                -- fillreturns = true,
-                -- nonewvars = true,
-                -- shadow = true,
-              },
-            },
-          },
-        },
-        graphql = {},
-        dartls = {
-          enabled = false,
-        },
-        biome = {},
-        html = {},
-        tailwindcss = {
-          settings = {
-            tailwindCSS = {
-              classFunctions = {
-                "tv",
-              },
-            },
-          },
-        },
-      },
-      setup = {
-        tailwindcss = function(_, opts)
-          opts.filetypes = opts.filetypes or {}
-          -- Add default filetypes
-          vim.list_extend(opts.filetypes, vim.lsp.config.tailwindcss.filetypes)
-          -- Remove excluded filetypes
-          --- @param ft string
-          opts.filetypes = vim.tbl_filter(function(ft)
-            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-          end, opts.filetypes)
-
-          -- Add additional filetypes
-          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
-        end,
       },
     },
   },
