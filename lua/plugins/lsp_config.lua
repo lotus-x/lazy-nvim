4ac176b59a7da8f23888044d6b9793b2b0da47a0
lotus-x <isuruimesh@gmail.com>
2025-10-20 19:56:02 +0530

    Removed tailwind hack

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -93,21 +93,6 @@ return {
           },
         },
       },
-      setup = {
-        tailwindcss = function(_, opts)
-          opts.filetypes = opts.filetypes or {}
-          -- Add default filetypes
-          vim.list_extend(opts.filetypes, vim.lsp.config.tailwindcss.filetypes)
-          -- Remove excluded filetypes
-          --- @param ft string
-          opts.filetypes = vim.tbl_filter(function(ft)
-            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-          end, opts.filetypes)
-
-          -- Add additional filetypes
-          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
-        end,
-      },
     },
   },
 }
