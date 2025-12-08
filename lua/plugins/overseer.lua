cd1ddae5f6959a7d64f36e5a9c8e92fc629512a9
lotus-x <isuruimesh@gmail.com>
2025-12-08 13:02:41 +0530

    Added overseer open output on task run

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -2,7 +2,14 @@ return {
   {
     "stevearc/overseer.nvim",
     opts = {
-      strategy = "toggleterm",
+      component_aliases = {
+        default = {
+          "on_exit_set_status",
+          "on_complete_notify",
+          { "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
+          { "open_output", direction = "float", focus = true },
+        },
+      },
     },
   },
 }
