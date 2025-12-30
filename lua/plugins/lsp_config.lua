e0ff08ce26aa320f7e1cc876429fac038554dca6
lotus-x <isuruimesh@gmail.com>
2025-12-30 11:00:54 +0530

    Removed vtsls formatting capabilities

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -46,6 +46,14 @@ return {
         },
         nushell = {},
       },
+      setup = {
+        vtsls = function()
+          Snacks.util.lsp.on({ name = "vtsls" }, function(_, client)
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+          end)
+        end,
+      },
     },
   },
 }
