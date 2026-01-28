fa13b99b6fff5ded7fff58941c9d14088a72ddfc
lotus-x <isuruimesh@gmail.com>
2026-01-28 20:28:50 +0530

    Added rose pine theme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    enabled = false,
+    opts = {
+      flavour = "latte",
+      -- flavour = "mocha",
+      transparent_background = true,
+      float = {
+        transparent = true,
+      },
+      auto_integrations = true,
+    },
+  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
+}
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,9 @@ return {
       opts.sections.lualine_c[4] = { LazyVim.lualine.pretty_path({
         length = 10,
       }) }
+
+      opts.options.theme = "rose-pine"
+
       return opts
     end,
   },
