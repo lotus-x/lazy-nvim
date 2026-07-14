d3fe74e530f6b7df1263418648e27303f40acd8c
lotus-x <isuruimesh@gmail.com>
2026-07-14 15:52:32 +0530

    Moved neovide font settings for native config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,36 +9,6 @@ vim.g.lazyvim_eslint_auto_format = false
 vim.g.lazyvim_prettier_needs_config = false
 
 if vim.g.neovide then
-  -- Set the font with fallback and size
-  vim.o.guifont = "Monaspace Neon,JetBrainsMono Nerd Font:h9"
-  vim.g.neovide_font_features = {
-    ["Monaspace Neon"] = {
-      "+calt",
-      "+dlig",
-      "+liga",
-      "+ss01",
-      "+ss02",
-      "+ss03",
-      "+ss04",
-      "+ss05",
-      "+ss06",
-      "+ss07",
-      "+ss08",
-    },
-    ["JetBrainsMono Nerd Font"] = {
-      "+calt",
-      "+ss01",
-      "+ss02",
-      "+ss03",
-      "+ss04",
-      "+ss05",
-      "+ss06",
-      "+ss07",
-      "+ss08",
-      "+liga",
-    },
-  }
-
   vim.opt.linespace = 1
   -- vim.g.neovide_opacity = 0.8
   -- vim.g.neovide_window_blur = true
@@ -46,7 +16,7 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0.08
   vim.g.neovide_cursor_trail_size = 0.4
   vim.g.neovide_cursor_vfx_mode = "railgun" -- Visual effect on cursor jump
-
+  --
   -- Normal, Visual, and Command modes paste via "+p
   vim.keymap.set({ "n", "v", "c" }, "<C-v>", '"+p', { silent = true })
   -- Insert mode needs <C-r>+ to paste text correctly without breaking typing flow
