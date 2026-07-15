3e1ed7811e295a30052e8458b278592db98ac2e5
lotus-x <isuruimesh@gmail.com>
2026-07-16 01:08:38 +0530

    Fixed neovide <C-V> like keybindings in terminal mode and insert mode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,9 +18,19 @@ if vim.g.neovide then
   vim.g.neovide_cursor_vfx_mode = "railgun" -- Visual effect on cursor jump
 
   -- Normal, Visual, and Command modes paste via "+p
-  vim.keymap.set({ "n", "v", "c" }, "<C-v>", '"+p', { silent = true })
+  -- vim.keymap.set({ "n", "v", "c" }, "<C-v>", '"+p', { silent = true })
   -- Insert mode needs <C-r>+ to paste text correctly without breaking typing flow
-  vim.keymap.set("i", "<C-v>", "<C-r>+", { silent = true })
+  -- vim.keymap.set("i", "<C-v>", "<C-r>+", { silent = true })
+  -- vim.keymap.set("t", "<C-S-v>", [[<C-\><C-n>"+pi]])
+  vim.keymap.set("t", "<C-S-v>", function()
+    local keys = vim.api.nvim_replace_termcodes('<C-\\><C-n>"+pi', true, false, true)
+    vim.api.nvim_feedkeys(keys, "n", false)
+  end)
+
+  local function paste()
+    vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+  end
+  vim.keymap.set({ "n", "i", "c", "t" }, "<C-v>", paste)
 end
 
 local sign = vim.fn.sign_define
