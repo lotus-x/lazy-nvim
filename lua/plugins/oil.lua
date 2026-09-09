78cf463fef94262bab240c05f9a8b7ca0e12393b
lotus-x <isuruimesh@gmail.com>
2026-09-09 11:57:26 +0530

    Added oil nvim toggle for showing system metadata

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -56,6 +56,17 @@ return {
           end,
           desc = "oil: Grep in directory",
         },
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
       },
       view_options = {
         show_hidden = true,
